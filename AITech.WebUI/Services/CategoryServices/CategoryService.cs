581fcbd5692f87451e4c3797a5f2026db8986bc8
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-11-26 18:07:26 +0300

    UI katmanında işlemler yapıldı.

---
+++ AITech.WebUI/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,32 @@
+﻿using AITech.WebUI.DTOs.CategoryDtos;
+
+namespace AITech.WebUI.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task CreateAsync(CreateCategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateCategoryDto> GetByAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(UpdateCategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
