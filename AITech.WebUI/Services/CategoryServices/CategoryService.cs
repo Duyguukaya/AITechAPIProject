450981143ae94d304826cbbc7e58d63eadc02e99
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-11-27 21:04:23 +0300

    UI tarafında kategori için listeleme işlemi tamamlandı.

--- AITech.WebUI/Services/CategoryServices/CategoryService.cs
+++ AITech.WebUI/Services/CategoryServices/CategoryService.cs
@@ -1,9 +1,19 @@
 ﻿using AITech.WebUI.DTOs.CategoryDtos;
+using Newtonsoft.Json;
+
 
 namespace AITech.WebUI.Services.CategoryServices
 {
     public class CategoryService : ICategoryService
     {
+        private readonly HttpClient _client;
+
+        public CategoryService(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://localhost:7012/api/");
+            _client = client;
+        }
+
         public Task CreateAsync(CreateCategoryDto categoryDto)
         {
             throw new NotImplementedException();
@@ -14,9 +24,21 @@ namespace AITech.WebUI.Services.CategoryServices
             throw new NotImplementedException();
         }
 
-        public Task<List<ResultCategoryDto>> GetAllAsync()
+        public async Task<List<ResultCategoryDto>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync("Categories");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Kategori listesi alınamadı");
+            }
+
+            var jsonContent = await response.Content.ReadAsStringAsync();
+
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonContent);
+
+            return values;
+
         }
 
         public Task<UpdateCategoryDto> GetByAsync(int id)
